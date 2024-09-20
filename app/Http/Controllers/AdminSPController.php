92df6c038131b07c4e52cc574c538d1e9a3c07af
nnh2706 <hungnguyen270604@gmail.com>
2024-09-20 23:09:50 +0700

    updatedb

--- app/Http/Controllers/AdminSPController.php
+++ app/Http/Controllers/AdminSPController.php
@@ -8,62 +8,61 @@ use App\Models\DanhMuc as danh_muc;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Pagination\Paginator;
+
 Paginator::useBootstrap();
 
 class AdminSPController extends Controller
 {
-    function index(Request $request){
+    function index(Request $request)
+    {
         $id_loai = -1;
         $perpage = env('PER_PAGE');
-        $loai_arr = danh_muc::all();    
+        $loai_arr = danh_muc::all();
+
         if ($request->has('id_dm')) {
             $id_loai = (int) $request['id_dm'];
-            $trangthai= 1; //chuaxoa, daxoa
+            //$trangthai = 1; //chuaxoa, daxoa
         }
-        if ($request->has('trangthai')){
+
+        if ($request->has('trangthai')) {
             $trangthai = $request['trangthai'];
-        }else {
+        } else {
             $trangthai = 1;
         }
-
-        if ($trangthai== 3){
-            $sanpham_arr = san_pham::onlyTrashed()->orderBy('masp','desc')
-            ->paginate($perpage)->withQueryString();
-            return view('admin/product_admin_delete',compact(['trangthai','id_loai','sanpham_arr','loai_arr']));
-        }
-        elseif ($trangthai == 2) {
+        if ($trangthai == 3) {
+            $sanpham_arr = san_pham::onlyTrashed()->orderBy('masp', 'desc')
+                ->paginate($perpage)->withQueryString();
+            return view('admin/product_admin_delete', compact(['trangthai', 'id_loai', 'sanpham_arr', 'loai_arr']));
+        } elseif ($trangthai == 2) {
             $sanpham_arr = san_pham::join('danh_muc', 'san_pham.id_dm', '=', 'danh_muc.id')
-            ->where('san_pham.trang_thai', 2)
-            ->orderBy('san_pham.id_dm', 'desc')
-            ->paginate($perpage)
-            ->withQueryString();
-            return view('admin/product_admin',compact(['trangthai','id_loai','sanpham_arr','loai_arr']));
-
-        }
-        else {
-            if ($id_loai>0){
-                $sanpham_arr = san_pham::join('danh_muc', 'san_pham.id_dm', '=', 'danh_muc.id')
-                ->select('san_pham.*', 'danh_muc.ten_dm')
-                ->orderBy('san_pham.id', 'desc')
-                ->where('san_pham.id_dm', $id_loai)
+                ->where('san_pham.trang_thai', 2)
+                ->orderBy('san_pham.id_dm', 'desc')
                 ->paginate($perpage)
                 ->withQueryString();
+            return view('admin/product_admin', compact(['trangthai', 'id_loai', 'sanpham_arr', 'loai_arr']));
+        } else {
+            if ($id_loai > 0) {
+                $sanpham_arr = san_pham::join('danh_muc', 'san_pham.id_dm', '=', 'danh_muc.id')
+                    ->select('san_pham.*', 'danh_muc.ten_dm')
+                    ->orderBy('san_pham.id', 'desc')
+                    ->where('san_pham.id_dm', $id_loai)
+                    ->paginate($perpage)
+                    ->withQueryString();
             } else {
                 $sanpham_arr = san_pham::join('danh_muc', 'san_pham.id_dm', '=', 'danh_muc.id')
-                ->select('san_pham.*', 'danh_muc.ten_dm')
-                ->orderBy('san_pham.id', 'desc')
-                ->where('san_pham.trang_thai',1)
-                ->paginate($perpage)
-                ->withQueryString();
+                    ->select('san_pham.*', 'danh_muc.ten_dm')
+                    ->orderBy('san_pham.id', 'desc')
+                    ->where('san_pham.trang_thai', 1)
+                    ->paginate($perpage)
+                    ->withQueryString();
             }
-            return view('admin/product_admin',compact(['trangthai', 'id_loai', 'sanpham_arr','loai_arr']));
+            return view('admin/product_admin', compact(['trangthai', 'id_loai', 'sanpham_arr', 'loai_arr']));
         }
     }
     public function create()
     {
-        $loai_arr = DB::table('danhmuc')->orderBy('madm' , 'asc')->get();
-        return view('admin.product_add_admin',compact('loai_arr'));
-    
+        $loai_arr = DB::table('danhmuc')->orderBy('madm', 'asc')->get();
+        return view('admin.product_add_admin', compact('loai_arr'));
     }
     public function store(Request $request)
     {
@@ -82,53 +81,56 @@ class AdminSPController extends Controller
             $obj->gia_km = 0;
         }
         $obj->id_dm = (int) $request['id_dm'];
-        $obj->ngay = now();  
+        $obj->ngay = now();
         // Lấy tệp tin từ trường input file
         if ($request->hasFile('anhsp')) {
             $file = $request->file('anhsp');
             $fileName = $file->getClientOriginalName();
             $file->move(public_path('/imgnew'), $fileName);
             $obj->anhsp = $fileName;
-        }        
+        }
         $obj->soluong = $request['soluong'];
         $obj->mo_ta_ngan = $request['mo_ta_ngan'];
-        $obj->mo_ta_ct = $request['mo_ta_ct']; 
+        $obj->mo_ta_ct = $request['mo_ta_ct'];
         $obj->save();
-        return redirect(route('san-pham.index'))->with('thongbao','Thêm thành công');
-    
+        return redirect(route('san-pham.index'))->with('thongbao', 'Thêm thành công');
     }
 
-    function khoiphuc($id) {
-            $sp = san_pham::withTrashed()->find($id);
-            if ($sp == null) return redirect('/thongbao');
-            $sp->restore();
-            return redirect('/admin/san-pham?trangthai=3');
+    function khoiphuc($id)
+    {
+        $sp = san_pham::withTrashed()->find($id);
+        if ($sp == null) return redirect('/thongbao');
+        $sp->restore();
+        return redirect('/admin/san-pham?trangthai=3');
     }
-    function xoavinhvien($id) {
+    function xoavinhvien($id)
+    {
         $sp = san_pham::withTrashed()->find($id);
         if ($sp == null) return redirect('/thongbao');
         $sp->forceDelete();
         return redirect('/admin/san-pham')->with('thongbao', 'Sản Phẩm Đã Được Xóa Vĩnh Viễn!');
     }
 
-    public function edit( Request $request , string $id) {
+    public function edit(Request $request, string $id)
+    {
         $sp = san_pham::where('masp', $id)->first();
-        if ($sp==null){
-            $request->session()->flash('thongbao','Không có sản phẩm này: '. $id);
+        if ($sp == null) {
+            $request->session()->flash('thongbao', 'Không có sản phẩm này: ' . $id);
             return redirect('/admin/sanpham');
         }
-        $loai_arr = DB::table('danhmuc')->orderBy('madm' , 'asc')->get();
-        return view('admin/product_edit_admin' , compact(['sp','loai_arr']));
+        $loai_arr = DB::table('danhmuc')->orderBy('madm', 'asc')->get();
+        return view('admin/product_edit_admin', compact(['sp', 'loai_arr']));
     }
 
-    public function update(Request $request, string $id) {
+    public function update(Request $request, string $id)
+    {
         $obj = san_pham::find($id);
         $obj->tensp = $request['tensp'];
-        $obj->slug = Str::slug($obj->tensp);     
+        $obj->slug = Str::slug($obj->tensp);
         $obj->gia = (int) $request['gia'];
         $obj->giakhuyenmai = (int) $request['giakhuyenmai'];
         $obj->madm = (int) $request['madm'];
-        $obj->ngay = $request['ngay']; 
+        $obj->ngay = $request['ngay'];
         $obj->anhsp = $request['anhsp'];
         $obj->soluong = $request['soluong'];
         $obj->motangan = $request['motangan'];
@@ -137,11 +139,11 @@ class AdminSPController extends Controller
         return redirect(route('san-pham.index'))->with('thongbao', 'Cập nhập thành công');
     }
 
-    public function destroy( Request $request, string $id) 
+    public function destroy(Request $request, string $id)
     {
         $cokhong = san_pham::where('masp', $id)->exists();
-        if ($cokhong==false) {
-            $request->session()->flash('thongbao','Sản phẩm không tồn tại');
+        if ($cokhong == false) {
+            $request->session()->flash('thongbao', 'Sản phẩm không tồn tại');
             return redirect('/admin/san-pham');
         }
         san_pham::where('masp', $id)->delete();
